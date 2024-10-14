3af23b815f77a7b2e390018fb442d5180c37fa3e
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-10-14 14:26:16 +0200

    fixed layer wms

--- src/components/mapprint/easyPrint/EasyPrint.jsx
+++ src/components/mapprint/easyPrint/EasyPrint.jsx
@@ -7,10 +7,24 @@ const EasyPrintControl = () => {
   useEffect(() => {
     if (routedMapRef && L.easyPrint) {
       const map = routedMapRef.leafletMap.leafletElement;
+      const wmsLayer = L.tileLayer
+        .wms("https://geodaten.metropoleruhr.de/spw2/service", {
+          layers: "spw2_light",
+          format: "image/png",
+          transparent: false,
+          version: "1.3.0",
+          crs: L.CRS.EPSG3857,
+          tiled: false,
+          width: 256,
+          height: 256,
+        })
+        .addTo(map);
       const browserControl = L.easyPrint({
         title: "Easy print",
         position: "topleft",
         sizeModes: ["A4Portrait"],
+        tileWait: 20000,
+        tileLayer: wmsLayer,
       }).addTo(map);
 
       return () => {
